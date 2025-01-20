2d56e6460ae603eb82473721066900e080618ba1
Incodi <frogeaterfromdoomland@gmail.com>
2025-01-20 12:57:07 -0500

    Added containerwithmostwater.py

---
+++ containerwithmostwater.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        l, r = 0, len(height)-1 # two pointers
+        distance = 0
+        # binary search like
+        while l < r: # Iterate through the array
+            distance = max(distance, min(height[l], height[r]) * (r - l))
+            # We want to keep the lines with taller height 
+            # because taller height can give a better max area.
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return distance
